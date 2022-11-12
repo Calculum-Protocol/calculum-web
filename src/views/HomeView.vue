9544783c33ef31ff3a389beaff387e2822f97676
David Grilli <djgrill@gmail.com>
2022-11-13 07:32:07 +0900

    updates GitBook link

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,7 +19,7 @@
             >
               <a href="/"><p><span class="text-yellow font-weight-bold">>_</span>Home</p></a>
               <a href="/" target="_blank"><p><span class="text-yellow font-weight-bold">>_</span>Vaults</p></a>
-              <a href="https://app.gitbook.com/o/sGAuGR9Uq2pGzhN0Y2im/home" target="_blank"><p><span class="text-yellow font-weight-bold">>_</span>Docs</p></a>
+              <a href="https://calculum.gitbook.io/calculum" target="_blank"><p><span class="text-yellow font-weight-bold">>_</span>Docs</p></a>
               <a href="https://medium.com" target="_blank"><p><span class="text-yellow font-weight-bold">>_</span>Blog</p></a>
             </div>
           </v-container>
